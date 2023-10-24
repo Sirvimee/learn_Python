01677e505ee1722c6319343ae2e2292489839205
Meeli <meeli.sirvi@tptlive.ee>
2023-10-24 20:08:17 +0300

    Version 5

--- EX/ex07_regex/regex.py
+++ EX/ex07_regex/regex.py
@@ -50,7 +50,7 @@ def find_sentences(text: str) -> list:
     :param text: given string to find sentences from
     :return: list of sentences found in given string
     """
-    pattern = r"[A-ZÕÜÖÄ][a-zõüöä,A-ZÕÜÖÄ:;\" -0-9]+[.!?]+"
+    pattern = r"[A-ZÕÜÖÄ][a-zõüöä,A-ZÕÜÖÄ:;\" \-\d]+[.!?]"
     return re.findall(pattern, text)
 
 
