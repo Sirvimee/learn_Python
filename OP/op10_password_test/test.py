02790d4733a308f1e59805974c8b129ebb76ee10
Meeli <meeli.sirvi@tptlive.ee>
2023-11-13 21:07:56 +0200

    Version 2

--- OP/op10_password_test/test.py
+++ OP/op10_password_test/test.py
@@ -3,18 +3,23 @@ import password
 
 
 def test__is_correct_length__too_short():
-    """Test whether password of length 7 is not correct."""
+    """Test whether password of length < 8 is not correct."""
+    assert password.is_correct_length("p") is False
     assert password.is_correct_length("passwor") is False
+    assert password.is_correct_length("pas") is False
 
 
 def test__is_correct_length__too_long():
     """Test whether password of length > 64 is incorrect."""
     assert password.is_correct_length("pass" * 18) is False
+    assert password.is_correct_length("p" * 65) is False
 
 
 def test__is_correct_length__correct_length():
-    """Test whether password of length 8 is correct."""
+    """Test whether password of length between 8 and 64 is correct."""
     assert password.is_correct_length("password") is True
+    assert password.is_correct_length("passwordaaaaa") is True
+    assert password.is_correct_length("p" * 64) is True
 
 
 def test__is_correct_length__empty():
@@ -35,6 +40,7 @@ def test__is_correct_length__correct_length_64():
 def test__includes_uppercase__no_uppercase():
     """Test whether password without uppercase is incorrect."""
     assert password.includes_uppercase("password") is False
+    assert password.includes_uppercase("lowercase") is False
 
 
 def test__includes_uppercase__with_uppercase():
@@ -45,6 +51,8 @@ def test__includes_uppercase__with_uppercase():
 def test__includes_uppercase__with_uppercase_and_lowercase():
     """Test whether password with uppercase and lowercase is correct."""
     assert password.includes_uppercase("PassWord") is True
+    assert password.includes_uppercase("Password123") is True
+    assert password.includes_uppercase("AbCdEfGhIjKlMnOpQrStUvWxYz") is True
 
 
 def test__includes_uppercase__empty():
@@ -84,12 +92,13 @@ def test__includes_lowercase__only_numbers():
 
 def test__includes_special__no_special():
     """Test whether password without special is incorrect."""
-    assert password.includes_special("Password") is False
+    assert password.includes_special("NoSpecial123") is False
 
 
 def test__includes_special__with_special():
     """Test whether password with special is correct."""
     assert password.includes_special("Password!") is True
+    assert password.includes_special("Hello@World") is True
 
 
 def test__includes_special__empty():
@@ -109,12 +118,13 @@ def test__includes_special__only_numbers():
 
 def test__includes_number__no_number():
     """Test whether password without number is incorrect."""
-    assert password.includes_number("Password!") is False
+    assert password.includes_number("NoNumbers!") is False
 
 
 def test__includes_number__with_number():
     """Test whether password with number is correct."""
-    assert password.includes_number("Password1") is True
+    assert password.includes_number("Password123") is True
+    assert password.includes_number("12345AbC") is True
 
 
 def test__includes_number__empty():
